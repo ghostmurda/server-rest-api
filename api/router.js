c2ebd8c0349242f964ea28e3eafd666fcd0695ab
Daniel Simonov <sk8kmd@gmail.com>
2020-10-11 14:40:20 +0300

    Add second endpoint, add simple pagination

--- api/router.js
+++ api/router.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const { followingList } = require('./users');
+const { popularList } = require("./users");
+const { getPopularList } = require("./users");
 
-router.get('/users', (req, res) => {
+router.get('/users/following', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
     res.json(followingList);
+});
+
+router.get('/users/popular', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+    let resJson = getPopularList(popularList, req.query.skip, req.query.pageSize);
+    res.json(resJson);
 });
 
 module.exports = router;
--- api/users.js
+++ api/users.js
@@ -11,4 +11,30 @@ let followingList = [
     {name: "Cydney Power", posts: "15", followers: "99", following: "6", info: "Test"},
 ];
 
-module.exports = {followingList};
+let popularList = [
+    {name: "Pavel Durov", posts: "3", followers: "4", following: "20", info: "Creator of VK and Telegram"},
+    {name: "Jon Snow", posts: "234", followers: "53", following: "5", info: "Son of Lyanna Stark and Rhaegar Targaryen"},
+    {name: "Test User", posts: "0", followers: "0", following: "100", info: "Test info"},
+    {name: "Dana West", posts: "427", followers: "568", following: "23", info: "I love myself"},
+    {name: "Roland Worthington", posts: "15", followers: "99", following: "6", info: "Worthington is like Washington but worth"},
+    {name: "Kelsi Clarkson", posts: "3", followers: "4", following: "20", info: "Test"},
+    {name: "Taiba Moran", posts: "234", followers: "4568", following: "5", info: "Test"},
+    {name: "Kason Parry", posts: "0", followers: "356", following: "100", info: "Test"},
+    {name: "Manha Hayden", posts: "427", followers: "1024", following: "23", info: "Test"},
+    {name: "Cydney Power", posts: "15", followers: "2009", following: "6", info: "Test"},
+].sort((a, b) => b.followers - a.followers);
+
+let getPopularList = function (arr, skip, pageSize){
+    let newArr = [];
+    let i = 0;
+    let j = skip * pageSize;
+    while (i < pageSize)
+    {
+        newArr.push(arr[j]);
+        i++;
+        j++;
+    }
+    return newArr;
+}
+
+module.exports = {followingList, popularList, getPopularList};
