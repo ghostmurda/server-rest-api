dee999929702bcf61fa50bb38ad7710bff063ce1
Daniel Simonov <sk8kmd@gmail.com>
2020-10-12 15:38:04 +0300

    Add new method getUser, refactor files

--- api/router.js
+++ api/router.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { followingList } = require('./users');
-const { popularList } = require("./users");
-const { getPopularList } = require("./users");
+const { followingList, popularList, getPopularList, getUser } = require('./users');
 
 router.get('/users/following', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -14,6 +12,11 @@ router.get('/users/popular', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
     let resJson = getPopularList(popularList, req.query.skip, req.query.pageSize);
+    res.json(resJson);
+});
+
+router.get('/users/profile', (req, res) => {
+    let resJson = getUser(popularList, req.query.userId);
     res.json(resJson);
 });
 
